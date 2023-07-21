b74805f6300c3bc6c6988646dffef84a7c1e1b7b
Leah <ldctaylor@gmail.com>
2023-07-22 09:49:42 +1000

    Order stories by publication date

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        return NewsStory.objects.all().order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
